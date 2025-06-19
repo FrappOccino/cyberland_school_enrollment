7a6a5d61609cae412e5f7618bb1a7781b14f875f
jeffrey wong <wongjeffreg@gmail.com>
2025-06-19 23:42:43 +0800

    export update

--- app/Exports/EnrollmentsExport.php
+++ app/Exports/EnrollmentsExport.php
@@ -4,14 +4,33 @@ namespace App\Exports;
 
 use App\Models\Enrollment;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class EnrollmentsExport implements FromCollection
+class EnrollmentsExport implements FromCollection, WithHeadings
 {
-    /**
-    * @return \Illuminate\Support\Collection
-    */
     public function collection()
     {
-        return Enrollment::all();
+        return Enrollment::select([
+            'child_name',
+            'child_birthday',
+            'lrn',
+            'parent_name',
+            'parent_contact',
+            'parent_email',
+            'parent_relationship',
+        ])->get();
+    }
+
+    public function headings(): array
+    {
+        return [
+            'Child Name',
+            'Birthday',
+            'LRN / Student ID',
+            'Parent Name',
+            'Parent Contact',
+            'Parent Email',
+            'Relationship',
+        ];
     }
 }
